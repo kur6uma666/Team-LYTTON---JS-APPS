e8d7817a1be2b70b6b0eb282c67c4ac4b0865f87
Rositsa <rozay@users.noreply.github.com>
2015-04-20 12:57:06 +0300

    added login-view.js

---
+++ Team-LYTTON/scripts/views/login-view.js
@@ -0,0 +1,17 @@
+var app = app || {};
+
+app.loginView = (function(){
+    function LoginView(selector, data) {
+        $.get('templates/login.html', function(template) {
+            var output = Mustache.render(template);
+
+            $(selector).html(output);
+        })
+    }
+
+    return {
+        load: function (selector, data) {
+            return LoginView(selector, data);
+        }
+    }
+});
